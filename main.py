7878ca6d6e16f74807186bc489e84531aaf90b88
James N <67208442+jamesnorbertowork@users.noreply.github.com>
2020-08-01 21:18:29 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+#Set's unordered, iterable, mutable and have no duplicate elements - Similar to lists, they have a major advantage ...
+#They have a highly optimized method for checking for elements in the set (a hash table)
+#Since sets are unordered, we cannot access items using indexes like we do in lists.
+
+# https://www.geeksforgeeks.org/sets-in-python/#:~:text=A%20Set%20is%20an%20unordered,mathematical%20notion%20of%20a%20set.&text=This%20is%20based%20on%20a%20data%20structure%20known%20as%20a%20hash%20table.
